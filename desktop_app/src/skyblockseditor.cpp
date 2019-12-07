24db80e3608f2f1e36a046b27171cf8ee7f47414
Aleksandr Postnikov <sasha2011-1997@mail.ru>
2019-12-07 06:10:08 +0300

    Add sending data in HTTP POST

--- desktop_app/src/skyblockseditor.cpp
+++ desktop_app/src/skyblockseditor.cpp
@@ -31,14 +31,8 @@ SkyBlocksEditor::SkyBlocksEditor(QWidget *parent)
     label.setPixmap(QPixmap::fromImage(image));
     ui->scrollArea->setWidget(&label);
 
-    /*QFont font = QFont("freeMono", 8);
-    QFontMetrics metrics = QFontMetrics(font);
-
-    qDebug() << metrics.lineSpacing() << endl << metrics.maxWidth() << endl;
-*/
     ui->scrollArea->setBackgroundRole(QPalette::Dark);
     connect(ui->sendCodeButton, SIGNAL(clicked()), this, SLOT(sendInformation()));
-    //connect(ui->sendCodeButton, SIGNAL(clicked()), this, SLOT(putMessage()));
 }
 
 SkyBlocksEditor::~SkyBlocksEditor()
@@ -50,9 +44,23 @@ void SkyBlocksEditor::sendInformation() {
     QString content = ui->codeEdit->toPlainText();
     QUrl url("http://localhost:6000");
 
-    QByteArray arr;
-    arr.append(content);
-    reply = networkManager.post(QNetworkRequest(url), arr);
+    QFont font = QFont("freeMono", 8);
+    QFontMetrics metrics = QFontMetrics(font);
+
+    QJsonObject obj;
+
+    obj["text"] = content;
+    obj["meta"] = QJsonObject({
+        QPair<QString, QJsonValue>({"line_spacing", QJsonValue(metrics.lineSpacing())}),
+        QPair<QString, QJsonValue>({"max_width", QJsonValue(metrics.maxWidth())})
+        });
+
+    QJsonDocument doc;
+    doc.setObject(obj);
+
+    qDebug() << QString(doc.toJson());
+
+    reply = networkManager.post(QNetworkRequest(url), doc.toJson());
     connect(reply, SIGNAL(finished()), this, SLOT(putMessage()));
 }
 
@@ -62,9 +70,10 @@ void SkyBlocksEditor::putMessage() {
         msg2 = reply->errorString();
         qDebug() << msg2 << endl;
     }
-    else
+    else {
         msg2 = QString(reply->readAll());
-
+        qDebug() << msg2;
+    }
     //msg = "{ \"figure\": [{\"size\": {\"width\": \"49\",\"height\": \"11\"},\"center\": {\"x\": \"105.0\",\"y\": \"7.5\"},\"text\": \"do smth\n\",\"page\": \"1\",\"type\": \"begincycle\"},{\"begin\": {\"x\": \"105.0\",\"y\": \"13.0\"},\"end\": {\"x\": \"105.0\",\"y\": \"18.0\"},\"text\": \"\",\"page\": \"1\",\"type\": \"line\"},{\"size\": {\"width\": \"77\",\"height\": \"11\"},\"center\": {\"x\": \"105\",\"y\": \"23.5\"},\"text\": \"do ano smth\n\",\"page\": \"1\",\"type\": \"endcycle\"}]}";
     //msg = "{\"figure\":[{\"size\":{\"width\":\"35\",\"height\":\"11\"},\"center\":{\"x\":\"105.000000\",\"y\":\"5.500000\"},\"text\":\"block\",\"page\":\"1\",\"type\":\"follow\"},{\"begin\":{\"x\":\"105.000000\",\"y\":\"11.000000\"},\"end\":{\"x\":\"105.000000\",\"y\":\"16.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"size\":{\"width\":\"56\",\"height\":\"22\"},\"center\":{\"x\":\"105.000000\",\"y\":\"27.000000\"},\"text\":\"cond\",\"page\":\"1\",\"type\":\"fork\"},{\"begin\":{\"x\":\"77.000000\",\"y\":\"27.000000\"},\"end\":{\"x\":\"52.500000\",\"y\":\"27.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"begin\":{\"x\":\"52.500000\",\"y\":\"27.000000\"},\"end\":{\"x\":\"52.500000\",\"y\":\"43.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"begin\":{\"x\":\"133.000000\",\"y\":\"27.000000\"},\"end\":{\"x\":\"157.500000\",\"y\":\"27.000000\"},\"text\":\"Да\",\"page\":\"1\",\"type\":\"line\"},{\"begin\":{\"x\":\"157.500000\",\"y\":\"27.000000\"},\"end\":{\"x\":\"157.500000\",\"y\":\"43.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"size\":{\"width\":\"28\",\"height\":\"11\"},\"center\":{\"x\":\"52.500000\",\"y\":\"48.500000\"},\"text\":\"smth\",\"page\":\"1\",\"type\":\"follow\"},{\"begin\":{\"x\":\"52.500000\",\"y\":\"54.000000\"},\"end\":{\"x\":\"52.500000\",\"y\":\"59.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"begin\":{\"x\":\"52.500000\",\"y\":\"43.000000\"},\"end\":{\"x\":\"52.500000\",\"y\":\"43.000000\"},\"text\":\"\",\"page\":\"1\",\"type\":\"line\"},{\"size\":{\"width\":\"21\",\"height\":\"11\"},\"center\":{\"x\":\"105.000000\",\"y\":\"48.500000\"},\"text\":\"end\",\"page\":\"1\",\"type\":\"follow\"}]}";
 
